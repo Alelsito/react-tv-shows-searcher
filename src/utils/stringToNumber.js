c95f46ba236a36884fc293faa9980aa0f07ab955
Alelsito <lbalelxander@gmail.com>
2022-08-30 23:50:40 -0500

    stringToNumber function for accordion (temporary)

---
+++ src/utils/stringToNumber.js
@@ -0,0 +1,48 @@
+
+const stringToNumber = (number) => {
+  if (number === 1) {
+    number = 'One'
+  } else if (number === 2) {
+    number = 'Two'
+  } else if (number === 3) {
+    number = 'Three'
+  } else if (number === 4) {
+    number = 'Four'
+  } else if (number === 5) {
+    number = 'Five'
+  } else if (number === 6) {
+    number = 'Six'
+  } else if (number === 7) {
+    number = 'Seven'
+  } else if (number === 8) {
+    number = 'Eight'
+  } else if (number === 9) {
+    number = 'Nine'
+  } else if (number === 10) {
+    number = 'Ten'
+  } else if (number === 11) {
+    number = 'Eleven'
+  } else if (number === 12) {
+    number = 'Twelve'
+  } else if (number === 13) {
+    number = 'Thirteen'
+  } else if (number === 14) {
+    number = 'Fourteen'
+  } else if (number === 15) {
+    number = 'Fifteen'
+  } else if (number === 16) {
+    number = 'Sixteen'
+  } else if (number === 17) {
+    number = 'Seventeen'
+  } else if (number === 18) {
+    number = 'Eighteen'
+  } else if (number === 19) {
+    number = 'Nineteen'
+  } else if (number === 20) {
+    number = 'Twenty'
+  }
+
+  return number
+}
+
+export default stringToNumber
