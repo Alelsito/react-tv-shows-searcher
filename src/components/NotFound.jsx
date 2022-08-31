3a4ea3ec943f9f525d41846d669eec383d6f74a1
Alelsito <lbalelxander@gmail.com>
2022-08-31 17:30:35 -0500

    improving Searcher and NotFound design

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,8 +4,29 @@ const NotFound = () => {
   return (
     <div className='container mt-5'>
       <div className='row'>
-        <div className='col'>
-          <h1 className='text-center'>404 Not Found</h1>
+        <div className='col d-flex justify-content-center'>
+          <div>
+            <div className='container container-ghost d-flex flex-column justify-content-center'>
+              <h1 className='text-error'>404</h1>
+              <h3 className='text-error-info'>NOT FOUND</h3>
+            </div>
+            <div className='container container-ghost'>
+              <div className='ghost-copy'>
+                <div className='one' />
+                <div className='two' />
+                <div className='three' />
+                <div className='four' />
+              </div>
+              <div className='ghost'>
+                <div className='face'>
+                  <div className='eye' />
+                  <div className='eye-right' />
+                  <div className='mouth' />
+                </div>
+              </div>
+              <div className='shadow' />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -12,14 +12,14 @@ const Searcher = ({ onCreate }) => {
     <div className='container d-flex flex-column'>
       <div className='row mb-3'>
         <div className='col'>
-          <h1 className='text-center'> ¿Qué serie deseas buscar? </h1>
+          <h1 className='text-center searcher-title'> ¿Qué serie deseas buscar? </h1>
         </div>
       </div>
       <form className='row' onSubmit={onSubmit}>
         <div className='col d-flex justify-content-center'>
           <input placeholder='Buscar' className='input-search' type='text' value={value} onChange={(e) => setValue(e.target.value)} />
           <button className='button-search' type='submit' value='buscar'>
-            <i className='fa fa-search' />
+            <i className='fa fa-search fa-lg' />
           </button>
         </div>
       </form>
